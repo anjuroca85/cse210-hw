3b6afe69da23333ae803c629cd2bea193a85a320
anjuroca85 <anjuroca85@gmail.com>
2024-02-28 11:42:40 -0500

    Prep 3 - Loops and guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //int magicNumber;
+        int guess;
+        //Console.Write("What's the magic number? ");
+        //magicNumber = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Random rnd = new Random();
+            int magicNumber = rnd.Next(1, 101);
+
+            do
+            {
+                Console.Write("What is your guess from 1 to 100? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    ++count;
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    ++count;
+                }
+                else
+                {
+                    count++;
+                    Console.WriteLine($"You guessed it in {count} times");
+                    count = 0;
+                    Console.WriteLine("Do you want to play again?");
+                    response = Console.ReadLine();
+                }
+            } while (guess != magicNumber);
+
+        }
     }
 }
