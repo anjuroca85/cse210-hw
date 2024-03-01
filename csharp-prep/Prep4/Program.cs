3b55a13f3dc4ce7e4c0260ca70162c1d4cc255db
anjuroca85 <anjuroca85@gmail.com>
2024-03-01 08:53:16 -0500

    Prep4 - Lists with Challenge

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,5 +1,5 @@
 using System;
-//using System.Globalization;
+using System.Globalization;
 using System.Collections.Generic;
 
 class Program
@@ -10,6 +10,7 @@ class Program
         int sum = 0;
         double avg = 0.0;
         int largest = -999999999;
+        int positiveNum = 999999999;
 
 
         List<int> numberList = new List<int>();
@@ -23,25 +24,42 @@ class Program
             numberList.Add(number);
         } while (number != 0);
 
-        //Console.Write($"{numberList}");
         foreach (int numberL in numberList)
         {
             if (numberL != 0)
             {
                 sum += numberL;
                 avg = (double)sum / (numberList.Count - 1);
+
+                if (numberL < positiveNum && numberL > 0)
+                {
+                    positiveNum = numberL;
+                }
+
                 if (numberL > largest)
                 {
                     largest = numberL;
                 }
             }
-
         }
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is: {avg}");
         //Console.WriteLine($"The average is: {avg:N15}");
         //Console.WriteLine($"The average is: {avg:F15}");
         Console.WriteLine($"The largest number is: {largest}");
+        if (positiveNum != 999999999)
+        {
+            Console.WriteLine($"The smallest positive number is: {positiveNum}");
+        }
 
+        Console.WriteLine("The sorted list is:");
+        numberList.Sort();
+        foreach (int num in numberList)
+        {
+            if (num != 0)
+            {
+                Console.WriteLine(num);
+            }
+        }
     }
 }
