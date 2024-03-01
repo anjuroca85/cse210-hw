682b85f62a35fef2e7b934fe1b91eb8b43b91e3a
anjuroca85 <anjuroca85@gmail.com>
2024-02-29 20:01:33 -0500

    Prep4 - Lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+//using System.Globalization;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number;
+        int sum = 0;
+        double avg = 0.0;
+        int largest = -999999999;
+
+
+        List<int> numberList = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            numberList.Add(number);
+        } while (number != 0);
+
+        //Console.Write($"{numberList}");
+        foreach (int numberL in numberList)
+        {
+            if (numberL != 0)
+            {
+                sum += numberL;
+                avg = (double)sum / (numberList.Count - 1);
+                if (numberL > largest)
+                {
+                    largest = numberL;
+                }
+            }
+
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        //Console.WriteLine($"The average is: {avg:N15}");
+        //Console.WriteLine($"The average is: {avg:F15}");
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
