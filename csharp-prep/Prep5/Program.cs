4c43af41d0e917f9bbabb6a8bebe75fdd8ddae9c
anjuroca85 <anjuroca85@gmail.com>
2024-03-01 16:16:20 -0500

    Prep5 - functions practice

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+        return favNum;
+    }
+
+    static int SquareNumber()
+    {
+        double squareNum = Math.Pow(PromptUserNumber(), 2);
+        int sqrtNum = (int)squareNum;
+        return sqrtNum;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.Write($"{name}, the square of your number is {number}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber());
     }
 }
