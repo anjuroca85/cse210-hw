0c9e5affc9eeac98119adf27afd25d0e460699f7
anjuroca85 <anjuroca85@gmail.com>
2024-03-07 19:59:56 -0500

    stubs for week 2 journal assigment

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+
+    public PromptGenerator() // constructor
+    {
+    }
+
+    //Method:
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+}
